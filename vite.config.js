4eef7684b0dab5b09ca924f74140d552b00e3811
༺TᕼᕮᖇᕮᗩᒪᘔᗩYᑎᕮ༻ <therealzayne@yahoo.com>
2026-04-03 20:31:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import restart from 'vite-plugin-restart'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import basicSsl from '@vitejs/plugin-basic-ssl'
@@ -7,7 +8,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 export default {
     root: 'sources/', // Sources files (typically where index.html is)
     envDir: '../',  // Directory where the env file is located
-    publicDir: '../', // Path from "root" to static assets (files that are served as they are)
+    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     base: './', // Public path (what's after the domain)
     server:
     {
@@ -25,6 +26,7 @@ export default {
     [
         wasm(),
         topLevelAwait(),
+        restart({ restart: [ '../static/**', ] }), // Restart server on static file change
         nodePolyfills(),
         // basicSsl()
     ]
